6c282d894a078631de954752142377661970766c
mixarik <mixarikhh@gmail.com>
2022-09-17 19:50:44 +0300

    study

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+class SwapiService {
+    _swapiBase = 'https://swapi.dev/api/';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    async GetInfo(url) {
+        const res = await fetch(`${this._swapiBase}${url}`);
+        if (!res.ok) {
+            throw new Error(`could not fetch${url} , recived ${res.status}`)
+        }
+        return await res.json();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    };
+
+   async getAllPiople (){
+     const res = await this.GetInfo('people/');
+    return res.results
+   };
+
+    getPiople (id){
+        this.GetInfo(`people/${id}/`);
+    };
+
+    async getAllPlanets (){
+        const res = await this.GetInfo('planets/');
+        return res.results
+    };
+
+    getPlanet (id){
+        this.GetInfo(`planets/${id}/`);
+    };
+
+    async getAllStarships (){
+        const res = await this.GetInfo('starships/');
+        return res.results
+    };
+
+    getStarship (id){
+        this.GetInfo(`starships/${id}/`);
+    };
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPiople().then((body) => body.forEach((item =>{
+    console.log(item.name)
+})));
+swapi.getPiople(2).then((body) => console.log(body));
